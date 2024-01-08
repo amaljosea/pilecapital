3504de025070b6642d54939061495548acf52923
Amal Jose <amaljoseami@gmail.com>
2024-01-08 17:09:50 +0530

    Empty message

--- components/TransferForm.tsx
+++ components/TransferForm.tsx
@@ -15,6 +15,9 @@ interface TransferFormProps {
   accounts: Account[];
 }
 
+const emptyMessage = (
+  <span className="text-red-800">This field is required</span>
+);
 const TransferForm: React.FC<TransferFormProps> = ({ onSubmit, accounts }) => {
   const {
     register,
@@ -27,68 +30,84 @@ const TransferForm: React.FC<TransferFormProps> = ({ onSubmit, accounts }) => {
   };
 
   return (
-    <form className="flex flex-col" onSubmit={handleSubmit(handleFormSubmit)}>
-      <label htmlFor="sourceAccount">Source Account:</label>
-      <select
-        id="sourceAccount"
-        {...register("sourceAccount", { required: true })}
-      >
-        {[
-          {
-            name: null,
-            id: undefined,
-          },
-          ...accounts,
-        ].map((account) => (
-          <option key={account.id} value={account.id}>
-            {account.name}
-          </option>
-        ))}
-      </select>
-      {errors.sourceAccount && <span>This field is required</span>}
+    <div className="p-5">
+      <h1 className="text-lg font-bold">Transfer Form</h1>
+      <form className="flex flex-col" onSubmit={handleSubmit(handleFormSubmit)}>
+        <label htmlFor="sourceAccount">Source Account:</label>
+        <select
+          className="border-solid border-2 border-indigo-600 rounded h-10"
+          id="sourceAccount"
+          {...register("sourceAccount", { required: true })}
+        >
+          {[
+            {
+              name: null,
+              id: undefined,
+            },
+            ...accounts,
+          ].map((account) => (
+            <option key={account.id} value={account.id}>
+              {account.name}
+            </option>
+          ))}
+        </select>
+        {errors.sourceAccount && emptyMessage}
 
-      <label htmlFor="amount">Amount (EUR):</label>
-      <input
-        type="number"
-        id="amount"
-        {...register("amount", { required: true })}
-      />
-      {errors.amount && <span>This field is required</span>}
+        <label htmlFor="amount">Amount (EUR):</label>
+        <input
+          className="border-solid border-2 border-indigo-600 rounded h-10"
+          type="number"
+          id="amount"
+          {...register("amount", { required: true })}
+        />
+        {errors.amount && emptyMessage}
 
-      <label htmlFor="recipientName">Recipient Name:</label>
-      <input
-        type="text"
-        id="recipientName"
-        {...register("recipientName", { required: true })}
-      />
-      {errors.recipientName && <span>This field is required</span>}
+        <label htmlFor="recipientName">Recipient Name:</label>
+        <input
+          className="border-solid border-2 border-indigo-600 rounded h-10"
+          type="text"
+          id="recipientName"
+          {...register("recipientName", { required: true })}
+        />
+        {errors.recipientName && emptyMessage}
 
-      <label htmlFor="targetIBAN">Target IBAN:</label>
-      <input
-        type="text"
-        id="targetIBAN"
-        {...register("targetIBAN", { required: true })}
-      />
-      {errors.targetIBAN && <span>This field is required</span>}
+        <label htmlFor="targetIBAN">Target IBAN:</label>
+        <input
+          className="border-solid border-2 border-indigo-600 rounded h-10"
+          type="text"
+          id="targetIBAN"
+          {...register("targetIBAN", { required: true })}
+        />
+        {errors.targetIBAN && (
+          <span className="text-red-800">This field is required</span>
+        )}
 
-      <label htmlFor="targetBIC">Target BIC:</label>
-      <input
-        type="text"
-        id="targetBIC"
-        {...register("targetBIC", { required: true })}
-      />
-      {errors.targetBIC && <span>This field is required</span>}
+        <label htmlFor="targetBIC">Target BIC:</label>
+        <input
+          className="border-solid border-2 border-indigo-600 rounded h-10"
+          type="text"
+          id="targetBIC"
+          {...register("targetBIC", { required: true })}
+        />
+        {errors.targetBIC && emptyMessage}
 
-      <label htmlFor="reference">Reference:</label>
-      <input
-        type="text"
-        id="reference"
-        {...register("reference", { required: true })}
-      />
-      {errors.reference && <span>This field is required</span>}
+        <label htmlFor="reference">Reference:</label>
+        <input
+          className="border-solid border-2 border-indigo-600 rounded h-10"
+          type="text"
+          id="reference"
+          {...register("reference", { required: true })}
+        />
+        {errors.reference && emptyMessage}
 
-      <button type="submit">Submit</button>
-    </form>
+        <button
+          type="submit"
+          className="mt-5 text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,12 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Link href="/transfer-form">transfer-form</Link>
+      <Link
+        href="/transfer-form"
+        className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+      >
+        Go to Transfer form
+      </Link>
     </main>
   );
 }
